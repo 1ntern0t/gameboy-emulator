7f75dcd7ef1b377582703c801fa1b6e533f6d03d
xirion <qvxyz9@yahoo.com>
2025-09-16 19:41:09 -0400

    Starting prototype

---
+++ gameboy.cpp
@@ -0,0 +1,18 @@
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+#include <SDL2/SDL.h>
+
+using namespace std::chrono;
+
+namespace Utils{}
+
+class MMU{};
+
+class PPU{};
+
+class CPU{};
+
+int main(int argc, char* argv[]){ return 0; }
